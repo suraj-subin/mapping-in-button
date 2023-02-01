5f411edc4155f42c0c3edbcfe3728f0484d81039
suraj-subin <surajsubin5@gmail.com>
2023-02-01 22:38:35 +0545

    mapping-in-button

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+
+export const category = [
+  {
+    id: 1,
+    heading: "Music",
+  },
+  {
+    id: 2,
+    heading: "Cable",
+  },
+  {
+    id: 3,
+    heading: "Charger",
+  },
+  {
+    id: 4,
+    heading: "Power",
+  },
+  {
+    id: 5,
+    heading: "Protection",
+  },
+  {
+    id: 6,
+    heading: "Holders",
+  },
+];
 
 function App() {
+  const [show, setShow] = React.useState(category);
+  // console.log("show is >>", show);
+  const filterItem = (categItem) => {
+    const updatedItems = category.filter((curElem) => {
+      return curElem === categItem;
+    });
+    setShow(updatedItems);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="Wrapper">
+        {/* <h1>[{show}]</h1>    <== Yasari Garda Error aauxa tesaile Ui maa dekhauna lai Aarko mapping lagayako */}
+        {category.map((info) => {
+          return (
+            <>
+              <button className="Btn" onClick={() => filterItem(info)}>
+                {info.heading}
+              </button>
+            </>
+          );
+        })}
+         {/* Yo Mapping chai UI maa dekhauna ko lagi ho. */}
+        {show.map((sur) => {
+          return (
+            <div key={sur.id} style={{ marginTop: "25px" }}>
+              <h1>{sur.heading}</h1>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
 
